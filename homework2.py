c45e8b406b28ea5aa8e04070b8c7b03c0c6d7a38
Kalophai <kalophai8@gmail.com>
2024-07-01 22:50:52 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+name = 'Artem'
+age = 22
+newAge = age + 1
+isStudent = True
+print('Name: ' + name)
+print("Age: " + str(age))
+print('New Age: ' + str(newAge))
+print('Is Student: ' + str(isStudent))
